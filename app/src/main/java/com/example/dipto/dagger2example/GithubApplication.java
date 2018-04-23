4c109d7272b5b6291b1d340ecd94bd4bc5c0f658
imtiaz <programmer.imtiaz@gmail.com>
2018-04-23 11:51:06 +0600

    DELETE- some commentout file deleted

--- app/src/main/java/com/example/dipto/dagger2example/GithubApplication.java
+++ app/src/main/java/com/example/dipto/dagger2example/GithubApplication.java
@@ -33,23 +33,6 @@ public class GithubApplication extends Application{
         picasso = component.getPiacasso() ;
 
 
-
-        /***** for checking is scope working well ******/
-
-        /*GithubService githubService2 = component.getGithubService();
-        Picasso picasso2 = component.getPiacasso() ;
-
-        GithubService githubService3 = component.getGithubService();
-        Picasso picasso3 = component.getPiacasso() ;
-
-        Log.i("DAGGER", "githubService : "+githubService) ;
-        Log.i("DAGGER", "githubService2 : "+githubService2) ;
-        Log.i("DAGGER", "githubService3 : "+githubService3) ;
-        Log.i("DAGGER", "picasso : "+picasso) ;
-        Log.i("DAGGER", "picasso2 : "+picasso2) ;
-        Log.i("DAGGER", "picasso3 : "+picasso3) ;*/
-
-        /***** for checking is scope working well ******/
     }
 
     /*public GithubApplicationComponent component() {
